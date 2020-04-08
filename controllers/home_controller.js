997732a42ce8bb588e9fde809c84894ee9e965b1
karun <karunahuja1998@gmail.com>
2020-04-09 01:49:45 +0530

    distributing code into partial

---
+++ controllers/home_controller.js
@@ -0,0 +1,32 @@
+const Post = require('../models/post');
+
+module.exports.home = function(req, res) {
+    // return res.end('<h1>Express is up for Codeial</h1>');
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        if(err){
+            console.log('Error in finding posts');
+            return;
+        }
+        if(!posts){
+            console.log('There seems to be no posts in the db');
+            return;
+        }
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    });
+
+};
