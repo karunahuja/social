07fac8934e1c0c9e6428edc42a7361f73a2098bf
karun <karunahuja1998@gmail.com>
2020-04-09 23:12:09 +0530

    adding more code to async await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,30 +1,38 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    Post.create({
+module.exports.create = async function(req, res){
+  try{
+    await Post.create({
         content: req.body.content,
         user: req.user._id
-    }, function(err, post){
-        if(err){console.log('error in creating a post'); return;}
-
-        return res.redirect('back');
     });
+        return res.redirect('back');
+}
+catch(err){
+    console.log(err,"error in creating post");
+    return;
+}
+    
 }
 
 
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
+module.exports.destroy = async function(req, res){
+    let post=await Post.findById(req.params.id)
         // .id means converting the object id into string
+        try{
         if (post.user == req.user.id){
             post.remove();
 
-            Comment.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            });
-        }else{
+           let comment=await Comment.deleteMany({post: req.params.id});
+            
+           return res.redirect('back');
+            }
+        else{
             return res.redirect('back');
+        }}
+        catch(err){
+            console.log(err,"error in destroying post")
         }
 
-    });
-}
+    };
