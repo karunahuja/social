06c7701276aa23efb9b952ec040bdb917b646c30
karun <karunahuja1998@gmail.com>
2020-04-11 04:24:21 +0530

    saving file from params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -12,12 +12,42 @@ module.exports.profile = function(req, res){
 }
 
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+   
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+
+        try{
+
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err) {console.log('*****Multer Error: ', err)}
+                
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
+
+
     }else{
+        req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 }
@@ -80,4 +110,4 @@ module.exports.destroySession = function(req, res){
     req.logout();
     req.flash('logout','logged out successfully');
     return res.redirect('/');
-}
+};
