82266d69b26fa124ce9761da3410dfc83775415e
karun <karunahuja1998@gmail.com>
2020-04-15 08:04:19 +0530

    beginning the initial connection

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware =require('node-sass-middleware');
 const flash=require('connect-flash');
 const customMware=require('./config/middleware');
+const chatServer=require('http').Server(app);
+const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening in 5000");
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
